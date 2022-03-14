671e404470a76bc765bb603b5518ee8d08dbeead
dmitryo <dmdon113@gmail.com>
2022-03-14 19:51:43 +0500

    Процедурный подход функция Draw

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,9 +10,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello WWWWWWooorrrllllddd");
+            //берем одну точку и символ
+            int x1 = 1;//задаем начальные координаты
+            int y1 = 3;
+            char sym1 = '*';//задаем символ вывода
+            Draw(x1, y1, sym1);
+
+            //берем вторую точку и символ
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
             Console.ReadLine();//ожидает нажатие кнопки
 
+        }
+        //для того чтобы код вывода точки не повторять, завдем функцию для этого действия 
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y); //выводим начало координат
+            Console.Write(sym);//выводим символ
         }
     }
 }
