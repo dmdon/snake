fc7891c1012d83b453ae2fc67b25db3e309f06f6
dmitryo <dmdon113@gmail.com>
2022-03-17 20:53:02 +0500

    Lesson 6. Осваиваем класс «Список»

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,10 +17,46 @@ namespace ConsoleApp1
 
             Point p2 = new Point(2, 6, '#');
             p2.Draw();
+
+            //добавить точки в список
+            Point p3 = new Point(10, 13, '%');
+            p1.Draw();
+
+            Point p4 = new Point(20, 60, '?');
+            p2.Draw();
+
+
+            //классы
+            List<int> numList = new List<int>();//переменная numList являетс переменной класса List, хранящих список целочисленных переменных
+            numList.Add( 0 ); //список целочисленных перменных
+            numList.Add( 1 );
+            numList.Add( 2 );
+
+            //для получения доступа к этим переменным надо знать порядковый номер
+            int x = numList[ 0 ];
+            int y = numList[ 1 ];
+            int z = numList[ 2 ];
+
+            //можно пройтись по всем значениям списка и вывести их на консоль, цикл foreach
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            //для удаления какого либо элемента используется remove
+            numList.RemoveAt(0);
+
+            //можно создать список содержащих несколько точек
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
             Console.ReadLine();//ожидает нажатие кнопки
 
         }
-        //для того чтобы код вывода точки не повторять, завдем функцию для этого действия 
+        //для того чтобы код вывода точки не повторять, заведем функцию для этого действия 
 
     }
 }
